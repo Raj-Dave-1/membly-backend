e1a292763620706937a2ce2a15b359d820cac04d
Raj Dave <raj.dave@talentsystems.com>
2023-09-12 01:06:22 +0530

    added graphana and prometheus

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 // Dada Ki Jay Ho
 
 import { log } from "console";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import promClient from "prom-client";
+import resTime from "response-time";
 
 import isAuth from "./middlewares/isAuth";
 import publicRoutes from "./routes/public";
@@ -18,22 +20,58 @@ import { createUsers, removeAllUsers } from "./scripts/add users";
 import isAdmin from "./middlewares/isAdmin";
 import adminRoutes from "./routes/admin";
 
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ register: promClient.register });
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// initialize redis database
+// REDIS: initialize redis database
 RedisClient.getInstance().getClient();
 
-app.use("/", (req, res, next) => {
+// Monitoring things
+const reqResTime = new promClient.Histogram({
+    name: "membly_backend_req_res_time",
+    help: "this is req-res time of memlby backend",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [0, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000],
+});
+const totalReqCounter = new promClient.Counter({
+    name: "membly_backend_total_req_counter",
+    help: "this is total number of request on memlby backend",
+});
+app.use(
+    resTime((req: Request, res: Response, time) => {
+        if (req.url != "/metrics") totalReqCounter.inc();
+        reqResTime
+            .labels({
+                method: req.method,
+                route: req.url,
+                status_code: req.statusCode,
+            })
+            .observe(time);
+    })
+);
+
+// Routse
+app.use("/", async (req, res, next) => {
     console.log(req.url, req.originalUrl);
     next();
 });
-app.use("/api", publicRoutes);
+
+// TODO: secure this and other api so that unauthorized can not call it
+app.get("/metrics", async (req: Request, res: Response) => {
+    res.setHeader("Content-Type", promClient.register.contentType);
+    const metrics = await promClient.register.metrics();
+    return res.send(metrics);
+});
+
+app.use("/", publicRoutes);
 app.use("/api", isAuth);
 app.use("/api/admin", isAdmin, adminRoutes);
 
-app.use("/", (req, res, next) => {
+app.use("/", async (req, res, next) => {
     return res.send("SitaRam");
 });
 
--- dist/app.js
+++ dist/app.js
@@ -18,6 +18,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const prom_client_1 = __importDefault(require("prom-client"));
+const response_time_1 = __importDefault(require("response-time"));
 const isAuth_1 = __importDefault(require("./middlewares/isAuth"));
 const public_1 = __importDefault(require("./routes/public"));
 const redis_client_1 = __importDefault(require("./utils/redis-client"));
@@ -29,12 +30,35 @@ collectDefaultMetrics({ register: prom_client_1.default.register });
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
-// initialize redis database
+// REDIS: initialize redis database
 redis_client_1.default.getInstance().getClient();
-app.use("/", (req, res, next) => {
+// Monitoring things
+const reqResTime = new prom_client_1.default.Histogram({
+    name: "membly_backend_req_res_time",
+    help: "this is req-res time of memlby backend",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [0, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000],
+});
+const totalReqCounter = new prom_client_1.default.Counter({
+    name: "membly_backend_total_req_counter",
+    help: "this is total number of request on memlby backend",
+});
+app.use((0, response_time_1.default)((req, res, time) => {
+    if (req.url != "/metrics")
+        totalReqCounter.inc();
+    reqResTime
+        .labels({
+        method: req.method,
+        route: req.url,
+        status_code: req.statusCode,
+    })
+        .observe(time);
+}));
+// Routse
+app.use("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(req.url, req.originalUrl);
     next();
-});
+}));
 // TODO: secure this and other api so that unauthorized can not call it
 app.get("/metrics", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.setHeader("Content-Type", prom_client_1.default.register.contentType);
@@ -44,10 +68,9 @@ app.get("/metrics", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
 app.use("/", public_1.default);
 app.use("/api", isAuth_1.default);
 app.use("/api/admin", isAdmin_1.default, admin_1.default);
-app.use("/", (req, res, next) => {
-    // createUsers();
+app.use("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     return res.send("SitaRam");
-});
+}));
 mongoose_1.default
     .connect(`mongodb://${config_1.MONGO_USER}:${config_1.MONGO_PASSWORD}@${config_1.MONGO_HOST}:${config_1.MONGO_PORT}/membly-mongo-database?authSource=admin`)
     .then(() => {
